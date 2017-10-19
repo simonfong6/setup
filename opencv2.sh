f21fd9fa790f68373bc114317a88f132c17517a1
Simon Fong <simonfong6@gmail.com>
2017-10-18 18:39:10 -0700

    Added cd home dir command

--- opencv2.sh
+++ opencv2.sh
@@ -1,3 +1,4 @@
+cd ~
 sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev
 libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev
 libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
