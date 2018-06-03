cd1f4287820ae8df3494df4ab3e33e85752ccc7d
Simon <simonfong6@gmail.com>
2018-06-02 22:21:14 -0700

    generic python script

---
+++ python/generic_python.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+generic_python.py
+
+Purpose
+"""
+
+class GenericPython:
+    
+    def __init__(self):
+        pass
+
+def _main():
+    from sys import argv
+    if(len(argv) < 2):
+        print("Not enough arguments.")
+        return
+    return
+    
+    
+if(__name__ == '__main__'):
+    _main()
